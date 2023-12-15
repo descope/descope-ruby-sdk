d8ac5eccc7fc7b3ae0e9d1ad10e27e699ac24abf
Ami Mahloof <ami@descope.com>
2023-12-14 22:37:22 -0500

    working!!!

--- examples/ruby-api.rb
+++ examples/ruby-api.rb
@@ -1,3 +1,9 @@
+# load the gem file for descope locally
+
 require 'descope'
 
-Descope::Client.new({project_id: "abc", management_key: "123"})
+c = Descope::Client.new({project_id: "P2OZcvKK1XtbyIlgYpdeqbRbtWwb", management_key: ENV['MGMT_KEY']})
+
+d = c.load(nil, "U2XuO2LSj1DGaUSnSgTWIEiWQIO8")
+# d = c.mgmt.user.load(nil, "U2XuO2LSj1DGaUSnSgTWIEiWQIO8")
+puts d["user"]["loginIds"]
