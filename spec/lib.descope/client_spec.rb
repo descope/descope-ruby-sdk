fc8c6a0f70b0e96fb403aa13ef9eeaefaaadc44a
Ami Mahloof <ami@descope.com>
2024-01-08 10:52:51 -0500

    fix rspec module name

--- spec/lib.descope/client_spec.rb
+++ spec/lib.descope/client_spec.rb
@@ -18,7 +18,7 @@ describe Descope::Client do
     it { should be_a Descope::Api::V1::Auth::Password }
     it { should be_a Descope::Api::V1::Auth::EnhancedLink }
     it { should be_a Descope::Api::V1::Auth::MagicLink }
-    it { should be_a Descope::Api::V1::Auth::Oauth }
+    it { should be_a Descope::Api::V1::Auth::OAuth }
   end
 
   let(:project_id) { 'P2AbcZVJYEUBGhX8LNjItawh5nAp' }
