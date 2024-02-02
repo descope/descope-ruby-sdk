543a469e597d8ecf9aa2c11416e361d1a6b8959c
Ami Mahloof <ami@descope.com>
2024-02-02 11:55:58 -0500

    audit spec

--- spec/integration/lib.descope/api/v1/management/audit_spec.rb
+++ spec/integration/lib.descope/api/v1/management/audit_spec.rb
@@ -8,4 +8,9 @@ describe Descope::Api::V1::Management::Audit do
   end
 
 
+  it 'should search the audit trail for user operations' do
+    res = @client.audit_search(actions: ['LoginSucceed'])
+    expect(res).to be_a(Hash)
+    expect(res['audits']).to be_a(Array)
+  end
 end
